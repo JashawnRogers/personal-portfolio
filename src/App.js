00f1321bac63a22fc9ee5097dd82e78e46c94d82
Jashawn <rogersjashawn20@gmail.com>
2022-11-17 00:14:59 -0700

    Created responsive navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <div className="text-white bg-black">
+    <div className="text-white bg-black h-screen overflow-hidden">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,42 @@
-import { FiSun } from 'react-icons/fi'
+import { useState } from "react"
 
-const navbar = () => {
+
+const Navbar = () => { 
+
+
+  const [ OpenMobileNav, setOpenMobileNav] = useState(false)
+
+  const navLinks = [
+    {name: 'Home', path: '/'},
+    {name: 'Projects', path: '/'},
+    {name: 'Contact', path: '/'},
+  ]
+
   return (
-    <nav className="border-b flex justify-between">
-        <div className="flex p-3">
-            <h1 className="">Jashawn Rogers</h1>
+    <nav className="flex justify-between min-w-full">
+        <div className="flex ">
+            <h1 className="md:text-5xl text-4xl p-4" style={{fontFamily: 'Staatliches'}}>Jashawn Rogers</h1>
         </div>
-        
-        <ul className="flex">
-          <li className="p-3 cursor-pointer  flex items-center hover:text-gray-800">
-              <a href="#">Home</a>
+        <button onClick={() => setOpenMobileNav(!OpenMobileNav)} className="md:hidden absolute right-6 p-3 z-[1000] text-5xl cursor-pointer">
+          <ion-icon name={OpenMobileNav ? 'close-outline' : 'menu-outline'}></ion-icon>
+        </button>
+        <ul className={ `${OpenMobileNav ? '': 'hidden'} md:flex md:items-center md:p-3 md:text-2xl md:static md:w-auto pt-20 pl-7 w-full h-auto absolute md:z-auto text-center transition-all duration-500 ease-in`} style={{fontFamily: 'Staatliches'}}>
+          {
+            navLinks.map((link) => (
+              <li key={link.name} className="md:p-3 cursor-pointer md:flex md:items-center py-5 text-2xl hover:text-gray-800">
+                <a href={link.path}>{link.name}</a>
+              </li>
+            ))
+          }
+          <li className="p-3 cursor-pointer flex justify-center  hover:text-gray-800 ">
+              <button className="text-3xl"><ion-icon name="sunny-outline"></ion-icon></button>
           </li>
-          <li className="p-3 cursor-pointer flex items-center hover:text-gray-800">
-              <a href="#">Projects</a>
-          </li>
-          <li className="p-3 cursor-pointer  flex items-center hover:text-gray-800">
-              <a href="#">Contact</a>
-          </li>
-          <li className="p-3 cursor-pointer  flex items-center hover:text-gray-800">
-              <button><FiSun/></button>
-          </li>
-
+          
+         
         </ul>
-        
+
     </nav>
   )
 }
 
-export default navbar
+export default Navbar
