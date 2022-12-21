a9e4d2cb6e358981c4deecccee2364108212f466
Jashawn <rogersjashawn20@gmail.com>
2022-12-20 21:51:50 -0700

    Added homepage to package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import MenuOverlay from "./components/MenuOverlay";
